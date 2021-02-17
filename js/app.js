dbb668d05a046d58314b33bf9cbb77a8a606ccf8
buchi-1996 <ukaigweonyebuchi@gmail.com>
2021-02-17 23:27:14 +0100

    new commit

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 const passwordViewToggler = document.querySelector('.eye-toggler');
 const passwordInput = document.querySelector('#password');
-const closeEyeIcon = 'fa-eye-slash';
+const openEyeIcon = 'fa-eye';
 let isOpen = true;
 
 const togglePasswordView = (e) => {
     isOpen = !isOpen;
    if(isOpen === false){
        passwordInput.type = 'text';
-       e.target.firstElementChild.classList.add(closeEyeIcon);
+       e.target.firstElementChild.classList.add(openEyeIcon);
    }else{
        passwordInput.type = 'password';
-       e.target.firstElementChild.classList.remove(closeEyeIcon);
+       e.target.firstElementChild.classList.remove(openEyeIcon);
    }
 }
 
