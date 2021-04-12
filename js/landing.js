34f263c12b6fa9ca81226e47e6636426e9d2d314
buchi-1996 <ukaigweonyebuchi@gmail.com>
2021-04-12 03:19:23 +0100

    new

--- js/landing.js
+++ js/landing.js
@@ -1,10 +1,12 @@
 const toggleMenu = document.querySelector('.toggle');
+const body = document.getElementById('landing-body');
 
 
 const toggleCourseModules = (e) => {
     console.log();
     const nav = e.target.parentElement.previousElementSibling;
     nav.classList.toggle('open');
+    body.classList.toggle('overflow')
 }
 
 toggleMenu.addEventListener('click', toggleCourseModules);
