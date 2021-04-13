c7c18af86f1fda5449ba48ecfe3f2231063dbf68
buchi-1996 <ukaigweonyebuchi@gmail.com>
2021-04-13 10:00:13 +0100

    made landing page

--- js/landing.js
+++ js/landing.js
@@ -1,12 +1,21 @@
 const toggleMenu = document.querySelector('.toggle');
+const icon = document.querySelector('.icon-menu');
 const body = document.getElementById('landing-body');
+let isOpen = false;
 
 
 const toggleCourseModules = (e) => {
-    console.log();
+    isOpen = !isOpen;
     const nav = e.target.parentElement.previousElementSibling;
     nav.classList.toggle('open');
-    body.classList.toggle('overflow')
+    body.classList.toggle('overflow');
+    console.log(isOpen);
+    if(isOpen){
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-times');
+    }else{
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+    }
 }
-
 toggleMenu.addEventListener('click', toggleCourseModules);
